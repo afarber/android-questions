e0094b2d61ec099fd2fc973b5f23c037884a12bf
Alexander Farber <Alexander.Farber@gmail.com>
2014-10-20 22:37:14 +0200

    Add DragAndDraw project by Big Nerd Ranch

---
+++ DragAndDraw/src/de/afarber/draganddraw/Box.java
@@ -0,0 +1,24 @@
+package de.afarber.draganddraw;
+
+import android.graphics.PointF;
+
+public class Box {
+    private PointF mOrigin;
+    private PointF mCurrent;
+
+    public Box(PointF origin) {
+        mOrigin = mCurrent = origin;
+    }
+
+    public PointF getCurrent() {
+        return mCurrent;
+    }
+
+    public void setCurrent(PointF current) {
+        mCurrent = current;
+    }
+
+    public PointF getOrigin() {
+        return mOrigin;
+    }
+}
---
+++ DragAndDraw/src/de/afarber/draganddraw/BoxDrawingView.java
@@ -0,0 +1,85 @@
+package de.afarber.draganddraw;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+
+import android.graphics.Canvas;
+
+import android.graphics.Paint;
+import android.graphics.PointF;
+
+import android.util.AttributeSet;
+
+import android.view.MotionEvent;
+import android.view.View;
+
+public class BoxDrawingView extends View {
+    private static final String TAG = "BoxDrawingView";
+
+    private ArrayList<Box> mBoxen = new ArrayList<Box>();
+    private Box mCurrentBox;
+    private Paint mBoxPaint;
+    private Paint mBackgroundPaint;
+
+    // used when creating the view in code
+    public BoxDrawingView(Context context) {
+        this(context, null);
+    }
+
+    // used when inflating the view from XML
+    public BoxDrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        // paint the boxes a nice semitransparent red (ARGB)
+        mBoxPaint = new Paint();
+        mBoxPaint.setColor(0x22ff0000);
+
+        // paint the background off-white
+        mBackgroundPaint = new Paint();
+        mBackgroundPaint.setColor(0xfff8efe0);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // fill the background
+        canvas.drawPaint(mBackgroundPaint);
+
+        for (Box box : mBoxen) {
+            float left = Math.min(box.getOrigin().x, box.getCurrent().x);
+            float right = Math.max(box.getOrigin().x, box.getCurrent().x);
+            float top = Math.min(box.getOrigin().y, box.getCurrent().y);
+            float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
+
+            canvas.drawRect(left, top, right, bottom, mBoxPaint);
+        }
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF curr = new PointF(event.getX(), event.getY());
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                // reset our drawing state
+                mCurrentBox = new Box(curr);
+                mBoxen.add(mCurrentBox);
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                if (mCurrentBox != null) {
+                    mCurrentBox.setCurrent(curr);
+                    invalidate();
+                }
+
+                break;
+
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                mCurrentBox = null;
+                break;
+        }
+        
+        return true;
+    }
+}
+
