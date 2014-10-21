c762427c87904a46e8bfd0e09a611f03e63ae50f
Alexander Farber <Alexander.Farber@gmail.com>
2014-10-21 15:39:02 +0200

    Add stubs for BigTile and SmallTile (the project does not compile)

---
+++ DragAndDraw/src/de/afarber/draganddraw/BigTile.java
@@ -0,0 +1,18 @@
+package de.afarber.draganddraw;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class BigTile extends View {
+	  // used when creating the view in code
+    public BigTile(Context context) {
+        this(context, null);
+    }
+
+    // used when inflating the view from XML
+    public BigTile(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ DragAndDraw/src/de/afarber/draganddraw/SmallTile.java
@@ -0,0 +1,18 @@
+package de.afarber.draganddraw;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class SmallTile extends View {
+	  // used when creating the view in code
+    public SmallTile(Context context) {
+        this(context, null);
+    }
+
+    // used when inflating the view from XML
+    public SmallTile(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
