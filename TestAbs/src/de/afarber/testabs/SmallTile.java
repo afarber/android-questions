2f05aefa7c83ea587d8ef6c8a1db63b504beaa5b
Alexander Farber <Alexander.Farber@gmail.com>
2014-10-23 13:19:16 +0200

    Add TestAbs project displaying "OK" and "Cancel" buttons

---
+++ TestAbs/src/de/afarber/testabs/SmallTile.java
@@ -0,0 +1,64 @@
+package de.afarber.testabs;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class SmallTile extends LinearLayout {
+	
+	private ImageView mImage;
+    private TextView mLetter;
+    private TextView mValue;
+
+    public SmallTile(Context context) {
+        this(context, null, null, null);
+    }
+
+    public SmallTile(Context context, AttributeSet attrs) {
+        this(context, attrs, null, null);
+    }
+    
+    public SmallTile(Context context, AttributeSet attrs, String letter, String value) {
+        super(context, attrs);
+        
+        this.setOrientation(VERTICAL);
+
+        mImage = new ImageView(context);
+        mImage.setImageResource(R.drawable.small_tile);
+        addView(mImage, new LinearLayout.LayoutParams(
+                LayoutParams.MATCH_PARENT, 
+                LayoutParams.MATCH_PARENT));
+
+        mLetter = new TextView(context);
+        mLetter.setText(letter);
+        addView(mLetter, new LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT, 
+                LayoutParams.WRAP_CONTENT));
+        
+        mValue = new TextView(context);
+        mValue.setText(value);
+        addView(mValue, new LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT, 
+                LayoutParams.WRAP_CONTENT));
+    }
+
+	public String getLetter() {
+		return mLetter.getText().toString();
+	}
+
+	public void setLetter(String letter) {
+        mLetter.setText(letter);
+	}
+
+	public String getValue() {
+		return mValue.getText().toString();
+	}
+
+	public void setValue(String value) {
+		mValue.setText(value);
+	}
+    
+    
+}
