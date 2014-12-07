4496c6ba708213787f60ecd00f5e980e23d8921b
Alexander Farber <Alexander.Farber@gmail.com>
2014-12-07 13:07:32 +0100

    Add PingMe big view notification example by Google

--- MyNotification/src/de/afarber/mynotification/MainActivity.java
+++ MyNotification/src/de/afarber/mynotification/MainActivity.java
@@ -1,6 +1,5 @@
 package de.afarber.mynotification;
 
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -9,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,42 +19,54 @@ import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 	private Context mContext;
-	private NotificationManager mManager;
+	private NotificationManager mNotificationManager;
+	private NotificationCompat.Builder mNotificationBuilder;
 	private static final int NOTIFY_ID = 1234;
+	
+	@Override
+	protected void onNewIntent(Intent intent) {
+		super.onNewIntent(intent);
+
+	    Bundle extras = getIntent().getExtras();
+	    if (extras != null) {
+	        Log.d("onNewIntent", "Extra: " + extras.getString("my_data"));
+	    }
+	}
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String question = getString(R.string.the_question);
 		mContext = getApplicationContext();
-		mManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+		mNotificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+		
+        Intent appIntent = new Intent(mContext, MainActivity.class);
+        appIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        appIntent.putExtra("my_data", 12345);
+
+		//Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://afarber.de"));
+		
+        PendingIntent contentIntent = PendingIntent.getActivity(mContext, 
+    		0, 
+    		appIntent, 
+    		PendingIntent.FLAG_UPDATE_CURRENT);
+        
+        mNotificationBuilder = new NotificationCompat.Builder(mContext)
+        	.setSmallIcon(R.drawable.ic_launcher)
+            .setContentTitle(question)
+            .setContentText(question)
+            .setTicker(question)
+            .setContentIntent(contentIntent)
+            .setAutoCancel(true);
         
 		Button showButton = (Button) findViewById(R.id.show);
 		showButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				openCar();
 
-		        Intent appIntent = new Intent(mContext, MainActivity.class);
-		        appIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-		        appIntent.putExtra("my_data", 12345);
-
-				//Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://afarber.de"));
-				
-		        String question = getString(R.string.the_question);
-		        PendingIntent contentIntent = PendingIntent.getActivity(mContext, 0, appIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-		        Notification notification = new NotificationCompat.Builder(mContext)
-		                .setContentTitle(question)
-		                .setContentText(question)
-		                .setTicker(question)
-		                .setWhen(System.currentTimeMillis())
-		                .setContentIntent(contentIntent)
-		                .setDefaults(Notification.DEFAULT_ALL)
-		                .setAutoCancel(true)
-		                .setSmallIcon(R.drawable.ic_launcher)
-		                .build();
-
-		        mManager.notify(NOTIFY_ID, notification);
+		        mNotificationManager.notify(NOTIFY_ID, mNotificationBuilder.build());
 			}
 		});
 		
@@ -63,17 +75,21 @@ public class MainActivity extends ActionBarActivity {
 			public void onClick(View v) {
 				closeCar();
 				
-				mManager.cancel(NOTIFY_ID);
+				mNotificationManager.cancel(NOTIFY_ID);
 			}
 		});
     }
     
     private void openCar() {
-		Toast.makeText(mContext, getString(R.string.car_opened), Toast.LENGTH_SHORT).show();
+		Toast.makeText(mContext, 
+				getString(R.string.car_opened), 
+				Toast.LENGTH_SHORT).show();
     }
 
     private void closeCar() {
-		Toast.makeText(mContext, getString(R.string.car_closed), Toast.LENGTH_SHORT).show();
+		Toast.makeText(mContext, 
+				getString(R.string.car_closed), 
+				Toast.LENGTH_SHORT).show();
     }
 
     @Override
