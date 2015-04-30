c7fe48a2e52e1b3daa2e36103ba429c1f0ff75db
Alexander Farber <Alexander.Farber@gmail.com>
2015-04-30 17:50:26 +0200

    Move shared prefs listener to PrefFragment

--- MyPrefs/src/de/afarber/myprefs/MainActivity.java
+++ MyPrefs/src/de/afarber/myprefs/MainActivity.java
@@ -3,29 +3,14 @@ package de.afarber.myprefs;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager.OnBackStackChangedListener;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 
-public class MainActivity 
-    extends Activity 
-	implements OnBackStackChangedListener, 
-			   OnSharedPreferenceChangeListener {
+public class MainActivity extends Activity implements OnBackStackChangedListener {
 	
-	public static final String BOOL_1 = "BOOL_1";
-	public static final String STR_1 = "STR_1";
-	public static final String STR_2 = "STR_2";
-	
-	private SharedPreferences mPrefs;	
-	private Editor mEditor;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,10 +18,6 @@ public class MainActivity
 	    getFragmentManager().addOnBackStackChangedListener(this);
         setContentView(R.layout.activity_main);
         
-		mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-		mPrefs.registerOnSharedPreferenceChangeListener(this);
-		mEditor = mPrefs.edit();
-		
 	    if (savedInstanceState == null) {
 	    	Fragment fragment = new MainFragment();
 	        getFragmentManager().beginTransaction()
@@ -83,16 +64,5 @@ public class MainActivity
 	@Override
 	public void onBackStackChanged() {
 		getActionBar().setDisplayHomeAsUpEnabled(getFragmentManager().getBackStackEntryCount() > 0);
-	}
-
-	@Override
-	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-			String key) {
-		Log.d("XXX", key);
-		
-		if (BOOL_1.equals(key)) {
-		} else if (STR_1.equals(key)) {
-		} else if (STR_2.equals(key)) {
-		} 		
 	}
 }
--- MyPrefs/src/de/afarber/myprefs/PrefFragment.java
+++ MyPrefs/src/de/afarber/myprefs/PrefFragment.java
@@ -1,12 +1,66 @@
 package de.afarber.myprefs;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
-public class PrefFragment extends PreferenceFragment {
+public class PrefFragment extends PreferenceFragment 
+    implements OnSharedPreferenceChangeListener {
+	
+	public static final String BOOL_1 = "bool_1";
+	public static final String STR_1 = "str_1";
+	public static final String STR_2 = "str_2";
+	
+	private CheckBoxPreference mBool1;
+	private EditTextPreference mStr1;
+	private EditTextPreference mStr2;
+	private SharedPreferences mPrefs;	
+	private Editor mEditor;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
-    	addPreferencesFromResource(R.xml.preferences);
+		setHasOptionsMenu(false);
+   		addPreferencesFromResource(R.xml.preferences);
+   		mBool1 = (CheckBoxPreference) findPreference(BOOL_1);
+   		mStr1 = (EditTextPreference) findPreference(STR_1);
+   		mStr2 = (EditTextPreference) findPreference(STR_2);
     }
+    
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		mPrefs = PreferenceManager.getDefaultSharedPreferences(activity);
+		mEditor = mPrefs.edit();
+		mPrefs.registerOnSharedPreferenceChangeListener(this);
+	}
+
+	@Override
+	public void onDetach() {
+		super.onDetach();
+		mPrefs.unregisterOnSharedPreferenceChangeListener(this);
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+		Log.d("onSharedPreferenceChanged", key);
+		
+		if (BOOL_1.equals(key)) {
+			boolean bool1 = prefs.getBoolean(key, false);
+			mBool1.setSummary(bool1 ? "Enabled" : "Disabled");
+		} else if (STR_1.equals(key)) {
+			String str1 = prefs.getString(key, "");
+			mStr1.setSummary(str1);
+		} else if (STR_2.equals(key)) {
+			String str2 = prefs.getString(key, "");
+			mStr2.setSummary(str2);
+		} 		
+	}    
 }
