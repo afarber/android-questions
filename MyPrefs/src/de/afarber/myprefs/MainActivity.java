2e965f76d2d580b73ca33484ada90375857b3f26
Alexander Farber <Alexander.Farber@gmail.com>
2015-04-30 15:51:42 +0200

    Add back stack listener, so that arrow is displayed in the action bar

--- MyPrefs/src/de/afarber/myprefs/MainActivity.java
+++ MyPrefs/src/de/afarber/myprefs/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends Activity implements OnBackStackChangedListener
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_ACTION_BAR);
+	    getFragmentManager().addOnBackStackChangedListener(this);
         setContentView(R.layout.activity_main);
 	    if (savedInstanceState == null) {
 	    	Fragment fragment = new MainFragment();
