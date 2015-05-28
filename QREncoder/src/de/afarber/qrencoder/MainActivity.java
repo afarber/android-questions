a8b203b1a45ebd2944f284a476ddd82310dd151c
Alexander Farber <Alexander.Farber@gmail.com>
2015-05-28 21:11:48 +0200

    Created QREncoder project (does not compile) using zxing

---
+++ QREncoder/src/de/afarber/qrencoder/MainActivity.java
@@ -0,0 +1,52 @@
+package de.afarber.qrencoder;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		ImageView imageView = (ImageView) findViewById(R.id.qrCode);
+		String qrData = "Data I want to encode in QR code";
+		int qrCodeDimention = 500;
+		QRCodeEncoder qrCodeEncoder = new QRCodeEncoder(qrData, 
+				null,
+		        Contents.Type.TEXT, 
+		        BarcodeFormat.QR_CODE.toString(), 
+		        qrCodeDimention);
+
+		try {
+		    Bitmap bitmap = qrCodeEncoder.encodeAsBitmap();
+		    imageView.setImageBitmap(bitmap);
+		} catch (WriterException e) {
+		    e.printStackTrace();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
