f66759f902df46a512c011998dce7d43f8b96288
Alexander Farber <Alexander.Farber@gmail.com>
2015-05-29 12:59:39 +0200

    Fixed QR-encoding of a String to Bitmap

--- QREncoder/src/de/afarber/qrencoder/MainActivity.java
+++ QREncoder/src/de/afarber/qrencoder/MainActivity.java
@@ -1,5 +1,8 @@
 package de.afarber.qrencoder;
 
+import java.util.EnumMap;
+import java.util.Map;
+
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,28 +10,55 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
 public class MainActivity extends AppCompatActivity {
+	
+	public final static int  WHITE = 0xFFFFFFFF;
+	public final static int  BLACK = 0xFF000000;
+	public final static int WIDTH = 500;
+	public final static String STR = "String to be encoded as QR code";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
 		ImageView imageView = (ImageView) findViewById(R.id.qrCode);
-		String qrData = "Data I want to encode in QR code";
-		int qrCodeDimention = 500;
-		QRCodeEncoder qrCodeEncoder = new QRCodeEncoder(qrData, 
-				null,
-		        Contents.Type.TEXT, 
-		        BarcodeFormat.QR_CODE.toString(), 
-		        qrCodeDimention);
-
 		try {
-		    Bitmap bitmap = qrCodeEncoder.encodeAsBitmap();
+		    Bitmap bitmap = encodeAsBitmap(STR);
 		    imageView.setImageBitmap(bitmap);
 		} catch (WriterException e) {
 		    e.printStackTrace();
 		}
+	}
+
+	Bitmap encodeAsBitmap(String str) throws WriterException {
+		Map<EncodeHintType,Object> hints = new EnumMap<EncodeHintType,Object>(EncodeHintType.class);
+		hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+		
+		BitMatrix result;
+		try {
+			result = new MultiFormatWriter().encode(str, BarcodeFormat.QR_CODE, WIDTH, WIDTH, hints);
+		} catch (IllegalArgumentException iae) {
+			// Unsupported format
+			return null;
+		}
+		int width = result.getWidth();
+		int height = result.getHeight();
+		int[] pixels = new int[width * height];
+		for (int y = 0; y < height; y++) {
+			int offset = y * width;
+			for (int x = 0; x < width; x++) {
+				pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
+			}
+		}
+		Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+		bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+		return bitmap;
 	}
 
 	@Override
