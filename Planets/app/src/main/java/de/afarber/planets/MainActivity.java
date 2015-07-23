a2a6b57e92f2b43038bbcdae244cbe2b252f3084
Alexander Farber <Alexander.Farber@gmail.com>
2015-07-24 00:28:45 +0200

    Add click handler to view holder

--- Planets/app/src/main/java/de/afarber/planets/MainActivity.java
+++ Planets/app/src/main/java/de/afarber/planets/MainActivity.java
@@ -1,6 +1,5 @@
 package de.afarber.planets;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,12 +9,29 @@ import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.Collections;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+    private class PlanetViewHolder
+            extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+        protected TextView text1;
+
+        public PlanetViewHolder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            text1 = (TextView) itemView.findViewById(android.R.id.text1);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(),
+                    "You have clicked " + ((TextView)itemView).getText(),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
     private RecyclerView mRecyclerView;
     private final String[] mPlanets = new String[] {
             "Mercury",
@@ -35,67 +51,31 @@ public class MainActivity extends AppCompatActivity {
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(new RecyclerView.Adapter<PlanetViewHolder>() {
 
-        RecyclerView.Adapter adapter = new PlanetAdapter(mPlanets, this);
-        mRecyclerView.setAdapter(adapter);
-/*
-            mRecyclerView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent,
-                                    View view,
-                                    int position,
-                                    long id) {
-                Toast.makeText(getApplicationContext(),
-                        "You have clicked " + mPlanets[position],
-                        Toast.LENGTH_LONG).show();
+            public PlanetViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View v = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+                PlanetViewHolder viewHolder = new PlanetViewHolder(v);
+                return viewHolder;
+            }
+
+            @Override
+            public void onBindViewHolder(PlanetViewHolder holder, int position) {
+                holder.text1.setText(mPlanets[position]);
+                holder.text1.setCompoundDrawablePadding(24);
+                holder.text1.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_stars_black_24dp, 0, 0, 0);
+            }
+
+            @Override
+            public int getItemCount() {
+                return mPlanets.length;
             }
         });
-*/
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         return false;
-    }
-}
-
-class PlanetAdapter extends RecyclerView.Adapter<PlanetAdapter.PlanetViewHolder> {
-
-    ArrayList<String> planetList = new ArrayList<>();
-
-    public PlanetAdapter(String[] planetList, Context context) {
-        Collections.addAll(this.planetList, planetList);
-    }
-
-    @Override
-    public PlanetAdapter.PlanetViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
-        PlanetViewHolder viewHolder = new PlanetViewHolder(v);
-        return viewHolder;
-    }
-
-    @Override
-    public void onBindViewHolder(PlanetAdapter.PlanetViewHolder holder, int position) {
-        holder.text1.setText(planetList.get(position).toString());
-
-        holder.text1.setCompoundDrawablePadding(24);
-        holder.text1.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_stars_black_24dp, 0, 0, 0);
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return planetList.size();
-    }
-
-    class PlanetViewHolder extends RecyclerView.ViewHolder {
-
-        protected TextView text1;
-
-        public PlanetViewHolder(View itemView) {
-            super(itemView);
-            text1 = (TextView) itemView.findViewById(android.R.id.text1);
-        }
     }
 }
