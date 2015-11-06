42982448acf8ddf96eee5e5a774a58f98e98b664
Alexander Farber <Alexander.Farber@gmail.com>
2015-11-06 23:10:26 +0100

    Use GridLayoutManager

--- WordGame/app/src/main/java/de/afarber/wordgame/ChooseLetterDialogFragment.java
+++ WordGame/app/src/main/java/de/afarber/wordgame/ChooseLetterDialogFragment.java
@@ -2,24 +2,22 @@ package de.afarber.wordgame;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class ChooseLetterDialogFragment extends DialogFragment {
 
     public final static String TAG = "ChooseLetterDialogFragment";
 
     private final static Character[] LETTERS = new Character[]{
-            '*', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
             'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
             'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
     };
@@ -82,7 +80,7 @@ public class ChooseLetterDialogFragment extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
         mRecyclerView = new RecyclerView(getContext());
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 4));
         mRecyclerView.setAdapter(new RecyclerView.Adapter<MyViewHolder>() {
 
             @Override
