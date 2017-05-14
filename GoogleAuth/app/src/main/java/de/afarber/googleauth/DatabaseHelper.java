c114e821625e61376e927af20a6a5751b669c3af
Alexander Farber <Alexander.Farber@gmail.com>
2017-05-14 20:32:01 +0200

    Add database helper and service

---
+++ GoogleAuth/app/src/main/java/de/afarber/googleauth/DatabaseHelper.java
@@ -0,0 +1,80 @@
+package de.afarber.googleauth;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+public class DatabaseHelper extends SQLiteAssetHelper {
+    private static final int DATABASE_VERSION    = 1;
+    private static final String DATABASE_NAME    = "social.db";
+    private static final String TABLE_SOCIAL     = "social";
+
+    public  static final String COLUMN_SID       = "sid";
+    public  static final String COLUMN_NET       = "net";
+    public  static final String COLUMN_GIVEN     = "given";
+    public  static final String COLUMN_FAMILY    = "family";
+    public  static final String COLUMN_PHOTO     = "photo";
+    public  static final String COLUMN_LAT       = "lat";
+    public  static final String COLUMN_LNG       = "lng";
+    public  static final String COLUMN_STAMP     = "stamp";
+
+    public static final int UNKNOWN       = 0;
+    public static final int GOOGLE        = 1;
+    public static final int APPLE         = 2;
+    public static final int ODNOKLASSNIKI = 4;
+    public static final int MAILRU        = 8;
+    public static final int VKONTAKTE     = 16;
+    public static final int FACEBOOK      = 32;
+
+    private static final String[] COLUMNS_SOCIAL = new String[] {
+            COLUMN_SID,
+            COLUMN_NET,
+            COLUMN_GIVEN,
+            COLUMN_FAMILY,
+            COLUMN_PHOTO,
+            COLUMN_LAT,
+            COLUMN_LNG,
+            COLUMN_STAMP
+    };
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+        if (!db.isReadOnly()) {
+            db.execSQL("pragma foreign_keys = on;");
+        }
+    }
+
+    private boolean findUser(int net) {
+        try (
+            Cursor cursor = getReadableDatabase().query(TABLE_SOCIAL,
+                    COLUMNS_SOCIAL,
+                    // OR: "net=" + net,
+                    "net=?",
+                    new String[]{String.valueOf(net)},
+                    null,
+                    null,
+                    null)) {
+
+            return (cursor.getCount() > 0);
+        }
+    }
+
+    public boolean findGoogleUser() {
+        return findUser(GOOGLE);
+    }
+
+/*
+    public User findNewestUser() {
+        return null;
+    }
+*/
+
+}
---
+++ GoogleAuth/app/src/main/java/de/afarber/googleauth/DatabaseService.java
@@ -0,0 +1,72 @@
+package de.afarber.googleauth;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+
+public class DatabaseService extends IntentService {
+    public static final String TAG = DatabaseService.class.getName();
+
+    public static final String ACTION_FIND_GOOGLE_USER = "de.afarber.action.find.google.user";
+    public static final String ACTION_GOOGLE_USER_EXISTS = "de.afarber.action.google.user.exists";
+    public static final String ACTION_GOOGLE_USER_MISSING = "de.afarber.action.google.user.missing";
+
+    public static final String ACTION_FIND_NEWEST_USER = "de.afarber.action.find.newest.user";
+    public static final String ACTION_NEWEST_USER_DATA = "de.afarber.action.newest.user.data";
+
+    private LocalBroadcastManager mBroadcastManager;
+    private DatabaseHelper mDatabaseHelper;
+
+    public static void findGoogleUser(Context context) {
+        Intent i = new Intent(context, DatabaseService.class);
+        i.setAction(ACTION_FIND_GOOGLE_USER);
+        context.startService(i);
+    }
+
+    public static void findNewestUser(Context context) {
+        Intent i = new Intent(context, DatabaseService.class);
+        i.setAction(ACTION_FIND_NEWEST_USER);
+        context.startService(i);
+    }
+
+    public DatabaseService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent i) {
+        if (i == null)
+            return;
+
+        mBroadcastManager = LocalBroadcastManager.getInstance(this);
+        mDatabaseHelper = new DatabaseHelper(this);
+
+        final String action = i.getAction();
+        if (ACTION_FIND_GOOGLE_USER.equals(action)) {
+            handleFindGoogleUser();
+        } else if (ACTION_FIND_NEWEST_USER.equals(action)) {
+            handleFindNewestUser();
+        }
+    }
+
+    public void onDestroy() {
+        mDatabaseHelper.close();
+        super.onDestroy();
+    }
+
+    private void handleFindGoogleUser() {
+        Intent i = new Intent();
+        i.setAction(mDatabaseHelper.findGoogleUser() ? ACTION_GOOGLE_USER_EXISTS : ACTION_GOOGLE_USER_MISSING);
+        mBroadcastManager.sendBroadcast(i);
+    }
+
+    private void handleFindNewestUser() {
+        Intent i = new Intent();
+        i.setAction(ACTION_NEWEST_USER_DATA);
+//        User user = mDatabaseHelper.findNewestUser();
+//        i.putExtra(DatabaseHelper.COLUMN_GIVEN, user.given);
+//        i.putExtra(DatabaseHelper.COLUMN_PHOTO, user.photo);
+        mBroadcastManager.sendBroadcast(i);
+    }
+}
