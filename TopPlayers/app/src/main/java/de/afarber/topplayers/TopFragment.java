b9966dccf5e590ed425162cde247e8961bd196fb
Alexander Farber <Alexander.Farber@gmail.com>
2018-10-30 15:07:20 +0100

    Implement the fragment (fetch JSON array of objects and store it into SQLite)

--- TopPlayers/app/src/main/java/de/afarber/topplayers/TopFragment.java
+++ TopPlayers/app/src/main/java/de/afarber/topplayers/TopFragment.java
@@ -1,34 +1,138 @@
 package de.afarber.topplayers;
 
+import android.app.Activity;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import com.mikepenz.fastadapter.FastAdapter;
+import com.mikepenz.fastadapter.adapters.ItemAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class TopFragment extends Fragment {
 
+    private final static String TAG = TopFragment.class.getSimpleName();
+    private final static String DATA = "data";
+
+    private final ItemAdapter<TopItem> mItemAdapter = new ItemAdapter<>();
+    private final FastAdapter<TopItem> mFastAdapter = FastAdapter.with(mItemAdapter);
+
     private TopViewModel mViewModel;
+    private ProgressBar mProgressBar;
+    private RecyclerView mRecyclerView;
+
+    private final Callback mCallback = new Callback() {
+        @Override
+        public void onFailure(@NonNull Call call, @NonNull IOException ex) {
+            Log.w(TAG, "onFailure", ex);
+            hideProgressBar();
+        }
+
+        @Override
+        public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+            hideProgressBar();
+            if (response.isSuccessful() && response.body() != null) {
+                String jsonStr = response.body().string();
+                Log.d(TAG, "onResponse jsonStr=" + jsonStr);
+                try {
+                    // parse the list of 30 top users and store them into SQLite
+                    List<TopEntity> tops = new ArrayList<>();
+                    JSONObject root = new JSONObject(jsonStr);
+                    JSONArray data = root.getJSONArray(DATA);
+                    for (int i = 0; i < data.length(); i++) {
+                        JSONObject jsonObj = data.getJSONObject(i);
+                        TopEntity top = new TopEntity(jsonObj);
+                        tops.add(top);
+                    }
+                    TopDatabase.getInstance(getActivity()).topDao().insertTops(tops);
+                } catch (JSONException | NullPointerException ex) {
+                    Log.w(TAG, "parsing top failed", ex);
+                }
+            }
+        }
+    };
 
     public static TopFragment newInstance() {
         return new TopFragment();
     }
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.top_fragment, container, false);
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(TopViewModel.class);
-        // TODO: Use the ViewModel
+        mViewModel.getTops().observe(this, tops -> {
+            mItemAdapter.clear();
+            for (TopEntity top: tops) {
+                TopItem item = new TopItem(top);
+                mItemAdapter.add(item);
+            }
+        });
+
+        View v = inflater.inflate(R.layout.top_fragment, container, false);
+        mProgressBar = v.findViewById(R.id.progressBar);
+        mRecyclerView = v.findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setAdapter(mFastAdapter);
+
+        fetchJsonData();
+
+        return v;
     }
 
+    private void showProgressBar() {
+        try {
+            mProgressBar.post(() -> mProgressBar.setVisibility(View.VISIBLE));
+        } catch (Exception ex) {
+            Log.w(TAG, "show progress bar failed", ex);
+        }
+    }
+
+    private void hideProgressBar() {
+        try {
+            mProgressBar.post(() -> mProgressBar.setVisibility(View.GONE));
+        } catch (Exception ex) {
+            Log.w(TAG, "show recycler view failed", ex);
+        }
+    }
+
+    private void fetchJsonData() {
+        showProgressBar();
+
+        FormBody.Builder bb = new FormBody.Builder();
+        Request request = new Request.Builder()
+                .url("https://slova.de/words/top.php")
+                .post(bb.build())
+                .build();
+
+        MainActivity.fetch(request, mCallback);
+    }
+
+    // {"data":[
+    // {"uid":4264,"elo":2467,"avg_time":"01:08","avg_score":16.7,"given":"Андрей","photo":"https://avt-1.foto.mail.ru/mail/andrej.kudinov.97/_avatarbig?1363079237"},
+    // {"uid":6042,"elo":2444,"avg_time":"00:40","avg_score":20.0,"given":"ЁЖ","photo":"https://avt-26.foto.mail.ru/mail/ez19311931/_avatarbig?1526102608"},
+    // {"uid":5176,"elo":2414,"avg_time":"00:32","avg_score":16.6,"given":"Сергей","photo":"https://avt-11.foto.mail.ru/mail/stangrit57/_avatarbig?1529002082"}
+    // ]}
 }
