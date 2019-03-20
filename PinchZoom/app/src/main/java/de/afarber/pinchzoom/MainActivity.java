442b0cb3b5025bad564920ce76521f56dadb6ba4
Alexander Farber <Alexander.Farber@gmail.com>
2019-03-20 10:34:38 +0100

    Add a custom view with scrolling and scaling enabled (however the zoom focus point is broken)

--- PinchZoom/app/src/main/java/de/afarber/pinchzoom/MainActivity.java
+++ PinchZoom/app/src/main/java/de/afarber/pinchzoom/MainActivity.java
@@ -1,10 +1,11 @@
 package de.afarber.pinchzoom;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+    protected final static String TAG = "PinchZoom";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
