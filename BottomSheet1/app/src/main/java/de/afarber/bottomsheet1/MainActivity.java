cec1d81c6ed278fca8cb95af0ae4b70c296e47a3
Alexander Farber <alexander.farber@volkswagen-infotainment.com>
2022-04-10 18:32:02 +0200

    Add 3 icons by Font Awesome, license CC 4.0

--- BottomSheet1/app/src/main/java/de/afarber/bottomsheet1/MainActivity.java
+++ BottomSheet1/app/src/main/java/de/afarber/bottomsheet1/MainActivity.java
@@ -1,43 +1,22 @@
 package de.afarber.bottomsheet1;
 
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
-import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class MainActivity extends AppCompatActivity {
-    private BottomSheetDialog mBottomSheetDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mBottomSheetDialog = new BottomSheetDialog(this);
-        mBottomSheetDialog.setContentView(R.layout.bottom_sheet_dialog);
-
-        Button button10 = mBottomSheetDialog.findViewById(R.id.button10);
-        Button button20 = mBottomSheetDialog.findViewById(R.id.button20);
-        Button button30 = mBottomSheetDialog.findViewById(R.id.button30);
-
-        button10.setOnClickListener(view -> closeBottomSheetDialog(view));
-        button20.setOnClickListener(view -> closeBottomSheetDialog(view));
-        button30.setOnClickListener(view -> closeBottomSheetDialog(view));
+        BottomSheetDialogFragment dialog = ModalBottomSheet.newInstance();
 
         Button mButton1 = findViewById(R.id.button1);
-        mButton1.setOnClickListener(view -> mBottomSheetDialog.show());
-    }
-
-    private void closeBottomSheetDialog(View view) {
-        Button button = (Button) view;
-        Toast.makeText(getApplicationContext(),
-                button.getText() + " is clicked",
-                Toast.LENGTH_SHORT).show();
-        mBottomSheetDialog.dismiss();
+        mButton1.setOnClickListener(view -> dialog.show(getSupportFragmentManager(), ModalBottomSheet.TAG));
     }
 }
---
+++ BottomSheet1/app/src/main/java/de/afarber/bottomsheet1/ModalBottomSheet.java
@@ -0,0 +1,41 @@
+package de.afarber.bottomsheet1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class ModalBottomSheet extends BottomSheetDialogFragment {
+    public static final String TAG = ModalBottomSheet.class.getName();
+
+    public static ModalBottomSheet newInstance() {
+        return new ModalBottomSheet();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.bottom_sheet_dialog, container, false);
+
+        v.findViewById(R.id.button10).setOnClickListener(view -> closeBottomSheetDialog(view));
+        v.findViewById(R.id.button20).setOnClickListener(view -> closeBottomSheetDialog(view));
+        v.findViewById(R.id.button30).setOnClickListener(view -> closeBottomSheetDialog(view));
+
+        return v;
+    }
+
+    private void closeBottomSheetDialog(View view) {
+        Button button = (Button) view;
+        Toast.makeText(requireContext(),
+                button.getText() + " is clicked",
+                Toast.LENGTH_SHORT).show();
+        dismiss();
+    }
+}
