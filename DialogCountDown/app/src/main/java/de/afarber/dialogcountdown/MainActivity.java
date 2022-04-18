b195ac7f1c2969c828201eb229dc5702eb7f8abd
Alexander Farber <farber72@outlook.de>
2022-04-18 14:51:09 +0200

    Change TextView to Button

--- DialogCountDown/app/src/main/java/de/afarber/dialogcountdown/MainActivity.java
+++ DialogCountDown/app/src/main/java/de/afarber/dialogcountdown/MainActivity.java
@@ -3,6 +3,8 @@ package de.afarber.dialogcountdown;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.show_dialog_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO
+            }
+        });
     }
 }
