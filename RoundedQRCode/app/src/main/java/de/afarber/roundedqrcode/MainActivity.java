f157601ebd8b5b2686af3ca2b29dddfe4b149736
Alexander Farber <farber72@outlook.de>
2022-04-30 14:10:59 +0200

    Display string in a toast and close the soft keyboard

--- RoundedQRCode/app/src/main/java/de/afarber/roundedqrcode/MainActivity.java
+++ RoundedQRCode/app/src/main/java/de/afarber/roundedqrcode/MainActivity.java
@@ -2,13 +2,34 @@ package de.afarber.roundedqrcode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView mImageView;
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mImageView = findViewById(R.id.imageView);
+        mEditText = findViewById(R.id.editText);
+        mEditText.setOnEditorActionListener((tv, actionId, event) -> {
+            String str = tv.getText().toString();
+            tv.setText(null);
+            if (!TextUtils.isEmpty(str)) {
+                Toast.makeText(getBaseContext(), str, Toast.LENGTH_LONG).show();
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(tv.getWindowToken(), 0);
+            }
+            return true;
+        });
     }
 }
