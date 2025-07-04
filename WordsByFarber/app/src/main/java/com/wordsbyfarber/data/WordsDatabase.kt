7150f9577cd9cd7b4774fb9a15efafac0581ec05
Alexander Farber <farber72@outlook.de>
2025-07-04 20:26:01 +0200

    Remove the outdated WordsByFarber

--- WordsByFarber/app/src/main/java/com/wordsbyfarber/data/WordsDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.wordsbyfarber.data
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [Words::class], exportSchema = false, version = 300)
-abstract class WordsDatabase : RoomDatabase() {
-    abstract fun wordsDao(): WordsDao
-}
