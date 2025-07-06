84590b118ef4835c2ba57329da65c8139a134151
Alexander Farber <farber72@outlook.de>
2025-07-06 22:29:22 +0200

    Revert "WordsByFarberApplication.kt was unnecessary"
    This reverts commit bb5c8699b3f4dfbe369aca8075599af47e18e450.

---
+++ WordsByFarber/app/src/main/java/com/wordsbyfarber/WordsByFarberApplication.kt
@@ -0,0 +1,11 @@
+package com.wordsbyfarber
+
+// Application class for global initialization and configuration
+import android.app.Application
+
+class WordsByFarberApplication : Application() {
+    
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
