11f91de39e98ca8892310d4a17ec7d0315fbb0f5
Alexander Farber <farber72@outlook.de>
2025-12-08 12:07:47 +0100

    Remove the old app

--- WordsByFarber/app/src/main/java/com/wordsbyfarber/domain/models/LanguageInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.wordsbyfarber.domain.models
-
-// Domain model representing language information for UI display
-data class LanguageInfo(
-    val code: String,
-    val displayName: String,
-    val flagIcon: String, // SVG path or resource identifier
-    val isSelected: Boolean = false
-)
--- WordsByFarber/app/src/main/java/com/wordsbyfarber/domain/models/WordSearchResult.kt
+++
@@ -1,8 +0,0 @@
-package com.wordsbyfarber.domain.models
-
-// Domain model representing the result of a word search operation
-data class WordSearchResult(
-    val word: String,
-    val explanation: String,
-    val isFound: Boolean
-)
